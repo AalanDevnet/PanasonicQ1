5d27431e9a74ac8d567f2ec114ca8bda74fc0c25
Alan <alan.burhanudin-Dotnet@outlook.com>
2024-07-02 19:10:07 +0700

    Panasonic

--- PanasonicQ1/Program.cs
+++ PanasonicQ1/Program.cs
@@ -10,6 +10,35 @@ namespace PanasonicQ1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Enter an integer: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 3 == 0 && number % 5 == 0)
+                    Console.WriteLine("DingDong");
+                else if (number % 3 == 0)
+                    Console.WriteLine("Ding");
+                else if (number % 5 == 0)
+                    Console.WriteLine("Dong");
+                else
+                    Console.WriteLine(number);
+
+                Console.Write("try again? (y/n): ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response == "n")
+                {
+                    Console.WriteLine("Application closing...");
+                    break;
+                }
+                else if (response != "y")
+                {
+                    Console.WriteLine("Invalid response. Application closing...");
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
